b75bbda503add8f40b17820c529383af35b1f5c5
Anatolii Zorin <Zorger27@GMail.com>
2026-06-10 22:26:32 +0300

    Small changes

--- src/store/info/infoSlice.js
+++ src/store/info/infoSlice.js
@@ -15,126 +15,132 @@ const initialState = [
   },
   {
     id: 3,
+    title: "Three.js Shading Language (TSL)",
+    version: "",
+    url: "https://github.com/mrdoob/three.js/wiki/Three.js-Shading-Language"
+  },
+  {
+    id: 4,
     title: "React Three Fiber",
     version: "9.6.1",
     url: "https://r3f.docs.pmnd.rs/getting-started/introduction"
   },
   {
-    id: 4,
+    id: 5,
     title: "React Three Drei",
     version: "10.7.7",
     url: "https://drei.docs.pmnd.rs/getting-started/introduction"
   },
   {
-    id: 5,
+    id: 6,
     title: 'React Router',
     version: '7.17.0',
     url: 'https://reactrouter.com',
   },
   {
-    id: 6,
+    id: 7,
     title: 'Redux Toolkit',
     version: '2.12.0',
     url: 'https://redux-toolkit.js.org',
   },
   {
-    id: 7,
+    id: 8,
     title: 'Vite',
     version: '8.0.16',
     url: 'https://vitejs.dev',
   },
   {
-    id: 8,
+    id: 9,
     title: 'React i18next',
     version: '17.0.8',
     url: 'https://react.i18next.com',
   },
   {
-    id: 9,
+    id: 10,
     title: "jsPDF",
     version: "4.2.1",
     url: "https://www.npmjs.com/package/jspdf"
   },
   {
-    id: 10,
+    id: 11,
     title: "HTML5",
     version: "",
     url: "https://developer.mozilla.org/ru/docs/Learn/HTML/Introduction_to_HTML"
   },
   {
-    id: 11,
+    id: 12,
     title: "CSS3",
     version: "",
     url: "https://developer.mozilla.org/ru/docs/Learn/CSS/First_steps"
   },
   {
-    id: 12,
+    id: 13,
     title: "SASS",
     version: "1.100.0",
     url: "https://sass-lang.com"
   },
   {
-    id: 13,
+    id: 14,
     title: "FlexBox CSS",
     version: "",
     url: "https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox"
   },
   {
-    id: 14,
+    id: 15,
     title: "Grid CSS",
     version: "",
     url: "https://developer.mozilla.org/ru/docs/Web/CSS/CSS_grid_layout"
   },
   {
-    id: 15,
+    id: 16,
     title: "JavaScript",
     version: "",
     url: "https://developer.mozilla.org/ru/docs/Learn/JavaScript"
   },
   {
-    id: 16,
+    id: 17,
     title: "Markdown",
     version: "",
     url: "https://www.markdownguide.org"
   },
   {
-    id: 17,
+    id: 18,
     title: "Font Awesome",
     version: "7.1.0",
     url: "https://fontawesome.com"
   },
   {
-    id: 18,
+    id: 19,
     title: "Google Search Console",
     version: "",
     url: "https://search.google.com/search-console"
   },
   {
-    id: 19,
+    id: 20,
     title: "Google Analytics",
     version: "4.0",
     url: "https://analytics.google.com"
   },
   {
-    id: 20,
+    id: 21,
     title: "Open Graph protocol",
     version: "",
     url: "https://ogp.me"
   },
   {
-    id: 21,
+    id: 22,
     title: "Favicon",
     version: "",
     url: "https://developer.mozilla.org/en-US/docs/Glossary/Favicon"
   },
   {
-    id: 22,
+    id: 23,
     title: "Sitemap",
     version: "",
     url: "https://www.sitemaps.org"
   },
   {
-    id: 23,
+    id: 24,
     title: "Robots.txt",
     version: "",
     url: "https://developers.google.com/search/docs/crawling-indexing/robots/intro"
